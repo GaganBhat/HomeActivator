721dcc8306d3fcb9e6188c588937cff373657b0f
GaganBhat <gaganbhat6@gmail.com>
2018-07-11 10:11:11 -0500

    Added TCP Client

--- app/src/main/java/com/example/gaganbhat/homeactivator/home_screen.java
+++ app/src/main/java/com/example/gaganbhat/homeactivator/home_screen.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -18,7 +19,7 @@ import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class home_screen extends Activity {
+public class home_screen extends Activity implements View.OnClickListener{
 
     private static String IP_ADDRESS = "192.168.1.11";
     private static int PORT = 5005;
@@ -44,28 +45,41 @@ public class home_screen extends Activity {
         relaySwitch = (Switch) findViewById(R.id.switchRelay);
         btnEnableRelay = (Button) findViewById(R.id.btnEnableRelay);
 
+        btnEnableRelay.setOnClickListener(this);
 
         Handler handler = new Handler();
         handler.postDelayed(r, 500);
 
         }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnEnableRelay:
+                try {
+                    sendToPort("EnableRelay");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
 
-
-    public static void sendToPort(String str) throws IOException {
+    public void sendToPort(String str) throws IOException {
         Socket socket = null;
         OutputStreamWriter osw;
-        System.out.println("RAN");
+        System.out.println("RAN-1");
         try {
             socket = new Socket("192.168.1.11", 5005);
             osw =new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
             System.out.println(str);
             osw.write(str, 0, str.length());
             osw.flush();
+            System.out.println("RAN-2");
         } catch (IOException e) {
             System.err.print(e);
         } finally {
-            if (!(socket == null)){
+            System.out.println("RAN-3");
+            if (socket != null) {
                 socket.close();
             }
         }
@@ -92,5 +106,6 @@ public class home_screen extends Activity {
             }
         }
     };
+
 
 }
